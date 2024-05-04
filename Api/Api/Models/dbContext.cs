841954ba909e281aa1addd1090761aab5c5804c8
Zedxel <ThomasTemby@hotmail.com>
2024-05-04 13:46:15 +1000

    Initial project setup

---
+++ Api/Api/Models/dbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Models;
+
+public partial class dbContext : DbContext
+{
+    public dbContext()
+    {
+    }
+
+    public dbContext(DbContextOptions<dbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=lotgdb");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
